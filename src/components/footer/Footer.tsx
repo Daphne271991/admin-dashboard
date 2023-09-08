7e6176d1ea88b2579c0e61ce62dd74338220dae8
Daphne271991 <134757720+Daphne271991@users.noreply.github.com>
2023-09-07 22:27:42 -0600

    finished homepage, added charts, top deals table

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>daphnedmin</span>
+      <span>© Daphne Dev Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
