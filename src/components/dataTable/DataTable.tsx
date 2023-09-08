94f9c6ed60135f8d27c33ddda2343d246b602779
Daphne271991 <134757720+Daphne271991@users.noreply.github.com>
2023-09-08 00:55:15 -0600

    Update DataTable.tsx

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -27,6 +27,7 @@ const DataTable = (props: Props) => {
   const handleDelete = (id: number) => {
     //delete the item
     // mutation.mutate(id)
+    const id = params.row.id;
   };
 
   const actionColumn: GridColDef = {
