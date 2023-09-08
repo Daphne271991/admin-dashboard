0a8c6d0b3707cd1f3f13c83981e8c0b15f2e7c66
Daphne271991 <134757720+Daphne271991@users.noreply.github.com>
2023-09-08 00:19:29 -0600

    Update DataTable.tsx

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -39,7 +39,6 @@ const DataTable = (props: Props) => {
           <Link to={`/${props.slug}/${params.row.id}`}>
             <img src="/view.svg" alt="" />
           </Link>
-
           <div className="delete" onClick={() => handleDelete(params.row.id)}>
             <img src="/delete.svg" alt="" />
           </div>
